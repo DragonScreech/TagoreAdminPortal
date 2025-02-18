7d48887e633242ad008988056a4602197fbc8035
DragonScreech <dragon.screech76@gmail.com>
2025-02-18 16:55:02 -0600

    changed upload page to root url

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
        
-        <Route path="/flyer-upload" element={<FlyerUpload />} />
+        <Route path="/" element={<FlyerUpload />} />
 
       </Routes>
     </BrowserRouter>
